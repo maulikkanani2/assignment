6b6dec6cdfb9564967c08cd5f57604e555d8b43f
Maulik <mk.globalia@gmail.com>
2022-02-23 14:52:04 +0530

    random days_practiced

--- app/serialzer.py
+++ app/serialzer.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 
 from app.models import Assignment
+import random
 
 class AssignmentSeializer(serializers.ModelSerializer):
+    days_practiced = serializers.IntegerField(required=False)
     class Meta:
         model = Assignment
         fields = "__all__"
+        
+    def create(self, validated_data):
+        validated_data['days_practiced'] = random.randint(1,validated_data['days'])
+        return super().create(validated_data)
